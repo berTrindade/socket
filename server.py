2e584b898a774c4748fbf2f92556d27774ce8466
Bernardo T. Abreu <berTrindade@users.noreply.github.com>
2018-11-10 00:11:26 -0200

    Update server.py

--- server.py
+++ server.py
@@ -18,17 +18,17 @@ sock.bind((hostname, 1234))
 # Tells the socket library that we want it to queue up as many as 5 connect requests (the normal max) before refusing outside connections
 sock.listen(5)
 
+(client, address) = sock.accept()
+
+print ("Got a connection from ", address," => Thanks...")
+
 while 1:
-    
-    (client, address) = sock.accept()
-    
-    print ("Got a connection from ", address," => Thanks...")
     
     response_from_client = client.recv(1024)
     
     print ("FROM CLIENT ===> ", response_from_client)
     
-    client.send(raw_input("TYPE A MESSAGE FOR CLIENT ==> ") + "\n")
+    client.send(raw_input("TYPE A MESSAGE FOR CLIENT ==> "))
     
 client.close()
 
